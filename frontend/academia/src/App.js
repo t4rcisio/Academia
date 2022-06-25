b9464986a8e279648598d3c7cc07f6198e08201c
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-06-25 14:40:51 -0300

    add a footer

--- frontend/academia/src/App.js
+++ frontend/academia/src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/academia/src/components/Footer/index.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import Link from "@mui/material/Link";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import "./style.css";
+
+function date() {
+  return new Date().getFullYear();
+}
+
+function Footer() {
+  return (
+    <footer>
+      <section className="main-footer">
+        <div className="cefet_logo">
+          <Link href="https://www.cefetmg.br/" underline="hover">
+            <Avatar
+              variant="square"
+              sx={{ width: 315, height: 90 }}
+              src="https://www.cefetmg.br/wp-content/uploads/2019/11/logo_topo.png"
+            ></Avatar>
+          </Link>
+        </div>
+
+        <div className="authors">
+          <Box
+            className="authors_links"
+            sx={{ "& > :not(style)": { m: 2 } }}
+            fontSize={14}
+          >
+            <Link
+              underline="hover"
+              color={"#FFFFFF"}
+              className="link"
+              href="https://www.linkedin.com/in/erickhenriquedds/"
+            >
+              <Avatar
+                style={{ margin: "10px" }}
+                src="https://github.com/ErickHDdS.png"
+              />
+              Erick Henrique
+            </Link>
+
+            <Link
+              underline="hover"
+              color={"#FFFFFF"}
+              className="link"
+              href="https://www.linkedin.com/in/iagor-s-486635138/"
+            >
+              <Avatar
+                style={{ margin: "10px" }}
+                src="https://github.com/IagorSs.png"
+              />
+              Iagor Souza
+            </Link>
+
+            <Link
+              underline="hover"
+              color={"#FFFFFF"}
+              className="link"
+              href="https://www.linkedin.com/in/vitor-laguardia-xavier-25200819b/"
+            >
+              <Avatar
+                style={{ margin: "10px" }}
+                src="https://github.com/vitor-laguardia.png"
+              />
+              Vitor Laguardia
+            </Link>
+
+            <Link
+              underline="hover"
+              color={"#FFFFFF"}
+              className="link"
+              href="https://www.linkedin.com/in/t4rcisio/"
+            >
+              <Avatar
+                style={{ margin: "10px" }}
+                src="https://github.com/t4rcisio.png"
+              />
+              Tarcisio Prates
+            </Link>
+          </Box>
+        </div>
+
+        <span className="infos">
+          Laboratório de Engenharia de Software I <br></br>
+          Copyright © {date()}
+        </span>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
