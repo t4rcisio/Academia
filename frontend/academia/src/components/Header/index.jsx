3cd99d26bd3ff4687c298bf917ca104812bd428d
Erick Henrique Dutra de Souza <55605746+ErickHDdS@users.noreply.github.com>
2022-06-25 15:11:01 -0300

    add a header

--- frontend/academia/src/components/Header/index.jsx
+++ frontend/academia/src/components/Header/index.jsx
@@ -1,9 +1,34 @@
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import Link from "@mui/material/Link";
+import PersonIcon from "@mui/icons-material/Person";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Box from "@mui/material/Box";
+
 import "./style.css";
 
 function Header() {
   return (
     <header>
-      <h1>Hello</h1>
+      <section className="main-header">
+        <Box className="name" sx={{ "& > :not(style)": { m: 3 } }}>
+          <Link underline="none" color={"#FFFFFF"} href="/">
+            <FitnessCenterIcon sx={{ fontSize: 50 }} />
+          </Link>
+
+          <Link underline="none" color={"#FFFFFF"} href="/">
+            Academia MFAL
+          </Link>
+        </Box>
+
+        <Box className="menu" sx={{ "& > :not(style)": { m: 3 } }}>
+          <Link underline="none" color={"#FFFFFF"} href="/">
+            <PersonIcon />
+          </Link>
+          <Link underline="none" color={"#FFFFFF"} href="/">
+            <LogoutIcon />
+          </Link>
+        </Box>
+      </section>
     </header>
   );
 }
