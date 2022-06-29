223ad43614f21139a987eb8e2fe60ba608dcc106
IagorSs <iagor.penaranda@uol.com.br>
2022-06-29 00:58:10 -0300

    Imeplement JWT system to login of users

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed, NotFound
 from .serializers import UserSerializer
 from .models import User
+import jwt, datetime
+
+from environ import Env
+
+env = Env()
 
 class RegisterView(APIView):
   def post(self, request):
@@ -25,4 +30,19 @@ class LoginView(APIView):
     if not user.check_password(password):
       raise AuthenticationFailed('Incorrect password')
 
-    return Response() # TODO return jwt here
+    payload = {
+      'id': user.id,
+      'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+      'iat': datetime.datetime.utcnow()
+    }
+
+    token = jwt.encode(payload, env('JWT_SECRET'), algorithm=env('JWT_ALGORITHM'))
+
+    response = Response()
+
+    response.set_cookie(key='jwt', value=token, httponly=True)
+    response.data = {
+      'jwt': token
+    }
+
+    return response
