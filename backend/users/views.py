5bbf451a8784006bde89791ec0aeb1e65afa7a6d
IagorSs <iagor.penaranda@uol.com.br>
2022-06-29 00:34:28 -0300

    Implement login route

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed, NotFound
 from .serializers import UserSerializer
+from .models import User
 
 class RegisterView(APIView):
   def post(self, request):
@@ -9,3 +11,18 @@ class RegisterView(APIView):
     serializer.save()
 
     return Response(serializer.data, status=201)
+
+class LoginView(APIView):
+  def post(self, request):
+    email = request.data['email']
+    password = request.data['password']
+
+    user = User.objects.filter(email=email).first()
+
+    if user is None:
+      raise NotFound(detail='User not found')
+
+    if not user.check_password(password):
+      raise AuthenticationFailed('Incorrect password')
+
+    return Response() # TODO return jwt here
