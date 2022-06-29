c4b4df66b10974329f2b9cb1c31fed8fb6d34291
IagorSs <iagor.penaranda@uol.com.br>
2022-06-29 00:43:05 -0300

    Add env variables to project

--- backend/Academia/settings.py
+++ backend/Academia/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from environ import Env
+
+env = Env()
+Env.read_env()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
